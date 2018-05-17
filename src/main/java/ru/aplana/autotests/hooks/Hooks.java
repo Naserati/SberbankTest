5c6a3c19c42d5c0cdf289fb2d77eab9d23903c82
ANazarov2 <alexander.nazarov@aplana.com>
2018-05-17 14:42:14 +0300

    Сбербанк

--- src/main/java/ru/aplana/autotests/hooks/Hooks.java
+++ src/main/java/ru/aplana/autotests/hooks/Hooks.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
-import util.TestProperties;
+import ru.aplana.autotests.util.TestProperties;
 
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
